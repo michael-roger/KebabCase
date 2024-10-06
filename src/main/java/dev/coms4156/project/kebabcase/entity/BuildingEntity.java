847bfa81508cc67c38f6a9fffd226d6a8d114504
Michael Roger <michael.roger@playonsports.com>
2024-10-06 15:21:18 -0400

    The base of the application.

---
+++ src/main/java/dev/coms4156/project/kebabcase/entity/BuildingEntity.java
@@ -0,0 +1,44 @@
+package dev.coms4156.project.kebabcase.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@Getter
+@Setter
+@NoArgsConstructor()
+@Entity(name = "Building")
+@Table(name = "buildings")
+public class BuildingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @Column(name = "created_datetime")
+    private OffsetDateTime createdDatetime;
+
+    @Column(name = "modified_datetime")
+    private OffsetDateTime modifiedDatetime;
+}
---
+++ src/main/java/dev/coms4156/project/kebabcase/entity/TokenEntity.java
@@ -0,0 +1,49 @@
+package dev.coms4156.project.kebabcase.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@Getter
+@Setter
+@NoArgsConstructor()
+@Entity(name = "Token")
+@Table(name = "Tokens")
+public class TokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "key")
+    private String key;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private ClientEntity client;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "expiration_datetime")
+    private OffsetDateTime expirationDatetime;
+
+    @Column(name = "created_datetime")
+    private OffsetDateTime createdDatetime;
+
+    @Column(name = "modified_datetime")
+    private OffsetDateTime modifiedDatetime;
+}
