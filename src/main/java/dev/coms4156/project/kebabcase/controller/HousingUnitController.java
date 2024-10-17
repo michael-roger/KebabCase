fb4a601b1744214314ac9356a8e0a200fcd6c23e
Elifia Muthia <elifiamuthia@Elifias-Air.lan>
2024-10-17 11:22:44 -0400

    implemented /housing-unit/{id} endpoint

--- src/main/java/dev/coms4156/project/kebabcase/controller/HousingUnitController.java
+++ src/main/java/dev/coms4156/project/kebabcase/controller/HousingUnitController.java
@@ -1,19 +1,23 @@
 package dev.coms4156.project.kebabcase.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import dev.coms4156.project.kebabcase.entity.BuildingEntity;
-import dev.coms4156.project.kebabcase.entity.HousingUnitEntity;
-import dev.coms4156.project.kebabcase.repository.BuildingRepositoryInterface;
-import dev.coms4156.project.kebabcase.repository.HousingUnitRepositoryInterface;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import dev.coms4156.project.kebabcase.entity.BuildingEntity;
+import dev.coms4156.project.kebabcase.entity.HousingUnitEntity;
+import dev.coms4156.project.kebabcase.repository.BuildingRepositoryInterface;
+import dev.coms4156.project.kebabcase.repository.HousingUnitRepositoryInterface;
 
 /** TODO */
 @RestController
@@ -23,6 +27,9 @@ public class HousingUnitController {
   private final BuildingRepositoryInterface buildingRepository;
 
   private final ObjectMapper objectMapper;
+
+  private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
 
   public HousingUnitController(
       HousingUnitRepositoryInterface housingUnitRepository,
@@ -56,5 +63,28 @@ public class HousingUnitController {
       Json.put("unit_number", housingUnit.getUnitNumber());
       return Json;
     }).collect(Collectors.toList());
+  }
+
+  @GetMapping("/housing-unit/{id}")
+  public ObjectNode getHousingUnit(@PathVariable int id) {
+
+    Optional<HousingUnitEntity> housingUnitRepoResult = this.housingUnitRepository.findById(id);
+
+    if (housingUnitRepoResult.isEmpty()) {
+      throw new ResponseStatusException(
+          HttpStatus.NOT_FOUND, "Housing unit with id " + id + " not found"
+      );
+    }
+
+    HousingUnitEntity unit = housingUnitRepoResult.get();
+
+    ObjectNode json = this.objectMapper.createObjectNode();
+    json.put("id", unit.getId());
+    json.put("building_id", unit.getBuilding().getId());
+    json.put("unit_number", unit.getUnitNumber());
+    json.put("created_datetime", unit.getCreatedDatetime().format(formatter));
+    json.put("modified_datetime", unit.getModifiedDatetime().format(formatter));
+
+    return json;
   }
 }
