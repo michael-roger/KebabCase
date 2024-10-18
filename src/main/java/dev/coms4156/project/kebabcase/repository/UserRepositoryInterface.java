a3e67888bd7a5f95a1f7f0ced421f4af56985853
Michael Roger <michael.roger@playonsports.com>
2024-10-18 19:37:16 -0400

    Fix all PMD errors.

--- src/main/java/dev/coms4156/project/kebabcase/repository/UserRepositoryInterface.java
+++ src/main/java/dev/coms4156/project/kebabcase/repository/UserRepositoryInterface.java
@@ -1,8 +1,5 @@
 package dev.coms4156.project.kebabcase.repository;
 
-import dev.coms4156.project.kebabcase.entity.BuildingEntity;
-import dev.coms4156.project.kebabcase.entity.HousingUnitEntity;
-import dev.coms4156.project.kebabcase.entity.HousingUnitFeatureEntity;
 import dev.coms4156.project.kebabcase.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
