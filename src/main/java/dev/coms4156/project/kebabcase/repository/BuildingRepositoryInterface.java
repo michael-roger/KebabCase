5a6802c9495699c237e9890d0a811bd500a60e3d
Toya Ora <t_ora@coloradocollege.edu>
2024-11-26 12:16:07 -0500

    remove used import

--- src/main/java/dev/coms4156/project/kebabcase/repository/BuildingRepositoryInterface.java
+++ src/main/java/dev/coms4156/project/kebabcase/repository/BuildingRepositoryInterface.java
@@ -1,7 +1,6 @@
 package dev.coms4156.project.kebabcase.repository;
 
 import dev.coms4156.project.kebabcase.entity.BuildingEntity;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
