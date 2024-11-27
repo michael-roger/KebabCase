9467937a2c69f899e4fedf19803f84ea81a087df
Michael Roger <michael.roger@playonsports.com>
2024-11-27 10:12:52 -0500

    KAN-79 - Disable CORS in Java Spring

--- src/main/java/dev/coms4156/project/kebabcase/config/SecurityConfig.java
+++ src/main/java/dev/coms4156/project/kebabcase/config/SecurityConfig.java
@@ -2,10 +2,9 @@ package dev.coms4156.project.kebabcase.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
